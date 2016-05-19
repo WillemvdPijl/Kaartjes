55c05c431046362b4cb58b95658b73d88ea9b2ee
Willem_Pijl <willemvanderpijl@hotmail.com>
2016-05-19 18:21:17 +0200

    Kleine update Kaartjes + geldautomaat

--- Geldautomaat.py
+++ Geldautomaat.py
@@ -1,12 +1,13 @@
 Automaat = "Aan"
 OpnameLimiet = 1500
 Voorraad = 1000
-Saldo = 25000
+Saldo = 1000
 
 def Run():
     print("Welkom bij ING")
-    print("Uw saldo bedraagt momenteel: €" + str(Saldo)"...")
-    o = int(input("Hoeveel geld wilt u opnemen?"))
+    print("Uw saldo bedraagt momenteel: €" + str(Saldo) + ".")
+    o = int(input("Hoeveel euro wilt u opnemen?: "))
+    NSaldo = Saldo - o
 
     if o < 0:
         print("Aanvraag mag niet negatief zijn, probeer opnieuw...\n")
@@ -14,7 +15,16 @@ def Run():
     elif (Saldo - o) < 0:
         print("Saldo niet toereikend... Ga geld verdienen man, skeere tata!\n")
         Run()
-    elif
+    elif (Saldo - o) >= 0:
+        print("U heeft €" + str(o) + " opgenomen. Uw saldo bedraagt momenteel: €" + str(NSaldo) + ".")
+        print("Bedankt voor het gebruiken van ING, nog een fijne dag\n")
+        Run()
+    elif (OpnameLimiet - o) < 0:
+        print("Dagelijkse opnamelimiet wordt overschreden, probeer opnieuw\n")
+        Run()
+    elif (Voorraad - o) < 0:
+        print("Biljetten voorraad niet toerekend, probeer opnieuw...\n")
+        Run()
 
 if Automaat == "Uit":
     print("Buiten bedrijf...")
